c535124a9022460c2169265dff1546584eb20527
Rajesh JVLN <rajeshj@appleseed.local>
2017-01-18 16:15:15 +0530

    Fixing jenkins build

--- basic-essentials/CHANGELOG.md
+++ basic-essentials/CHANGELOG.md
@@ -1,4 +1,4 @@
 # 0.1.0
 [rajeshj] - Initial release of oracle java - 1.8.0_u112
 #0.1.1
-[rajeshj] - New jenkins changes tested
+[rajeshj] - Updated attributes for passing build
--- basic-essentials/attributes/default.rb
+++ basic-essentials/attributes/default.rb
@@ -1,7 +1,7 @@
 default['oracle_java']['app']['base_directory']='/usr/lib/jvm'
 default['oracle_java']['app']['default_path']= '/usr/bin/java'
 
-#Oracle Java - 8u111 - Default version
+#Oracle Java
 
 default['oracle_java']['default']['binary_version']='8u111'
 default['oracle_java']['default']['binary_package']="jdk-#{node['oracle_java']['default']['binary_version']}-linux-x64.tar.gz"
@@ -11,17 +11,17 @@ default['oracle_java']['default']['app_version']='1.8.0_111'
 default['oracle_java']['default']['home_directory']="#{node['oracle_java']['app']['base_directory']}/jdk#{node['oracle_java']['default']['app_version']}"
 default['oracle_java']['default']['binary_path']="#{node['oracle_java']['default']['home_directory']}/jre/bin/java"
 
-
+#Git Client
 default['gitclient']['pin_version']=true
 default['gitclient']['binary']['packages'] = {
   'amazon' => {
     '2016.09' => {
-        'git' => '2.7.4-1.47.amzn1'
+      'git' => '2.7.4-1.47.amzn1'
     }
   },
   'centos' => {
     '7.2.1511' => {
-        'git' => '1.8.3.1-6.el7_2.1'
+      'git' => '1.8.3.1-6.el7_2.1'
     },
     '7.3.1611' => {
       'git' => '1.8.3.1-6.el7_2.1'
@@ -29,15 +29,14 @@ default['gitclient']['binary']['packages'] = {
 },
   'ubuntu' => {
     '14.04' => {
-        'git' => '1.9.1-1ubuntu0.3'
+      'git' => '1.9.1-1ubuntu0.3'
     },
     '16.04' => {
       'git' => '1.9.1-1ubuntu0.3'
     }
-
   }
 }
-
+#ChefDK
 default['chefdk']['binary']['version']='1.1.16'
 default['chefdk']['binary']['package'] = {
     'rhel' => "chefdk-#{node['chefdk']['binary']['version']}-1.el7.x86_64.rpm",
@@ -54,6 +53,7 @@ default['chefdk']['download']['uri']= {
         '16.04' => "https://packages.chef.io/files/stable/chefdk/#{node['chefdk']['binary']['version']}/#{node['platform']}/#{node['platform_version']}/#{node['chefdk']['binary']['package'][node['platform_family']]}"
     }
 }
+#EPEL repo
 case node['platform_version']
   when '7.2.1511', '7.3.1611', '2016.09'
     default['epel']['repo']['uri']='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
