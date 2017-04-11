7132ac02b3e713ea77f32eb10a3dd97ad2ff41a8
Venkat Bumireddy <venkat.bumireddy@phenompeople.com>
2017-04-11 15:38:40 +0530

    data intial import

---
+++ data-service/recipes/ecr_deploy.rb
@@ -0,0 +1,4 @@
+ecr_deploy "ph_data-service Deploy" do
+  vault_name 'services'
+  app_name 'ph_data-service'
+end
---
+++ data-service/recipes/ecr_undeploy.rb
@@ -0,0 +1,4 @@
+ecr_undeploy "ph_data-service undeploy" do
+  vault_name 'services'
+  app_name 'ph_data-service'
+end
