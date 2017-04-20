04f0f543518467f2c70c5fcf9e48f9a3c01bcd32
Rajesh Jonnalagadda <rajesh.jonnalagadda@phenompeople.com>
2017-04-20 11:56:18 +0530

    Updating cve scanner

--- bash/src/docker_build.sh
+++ bash/src/docker_build.sh
@@ -4,24 +4,23 @@ WHATPUSHED=$(git whatchanged -1 --format=%f $REPO_BASE_DIRECTORY | grep '^:' | c
 for EACH_CHANGE in $WHATPUSHED; do
     BASE_NAME=$(basename $EACH_CHANGE)
     BASE_PATH=$(dirname  $EACH_CHANGE)
-    if [ "$BASE_NAME" == 'Dockerfile' ]; then
-        if [ -f $EACH_CHANGE ]; then
-            echo -e "\x1B[01;32mFound Docker configuration file at $EACH_CHANGE \x1B[0m"
-            BASE_PATH=$(echo $BASE_PATH | sed 's#$REPO_BASE_DIRECTORY/##')
-            read ORGANISATION REPO_NAME IMAGE_TAG <<< $(echo $BASE_PATH | awk -F'[/:]' '{print $1,$2,$3}')
-            echo -e "\x1B[01;32mBuilding container from configuration file $EACH_CHANGE with tag $ORGANISATION/$REPO_NAME:$IMAGE_TAG \x1B[0m"
-            BUILD_NAME="$ORGANISATION/$REPO_NAME:$IMAGE_TAG"
-            docker build -t $BUILD_NAME $BUILD_NAME
-            LATEST_IMAGE=$(ls -rd "$REPO_BASE_DIRECTORY/$ORGANISATION/$REPO_NAME"* | head -1)
-            CURRENT_IMAGE="$REPO_BASE_DIRECTORY/$BUILD_NAME"
-            if [ "$LATEST_IMAGE" == "$CURRENT_IMAGE" ]; then
-                echo -e "\x1B[01;32m Updating latest tag for $ORGANISATION/$REPO_NAME \x1B[0m"
-                docker tag $BUILD_NAME $ORGANISATION/$REPO_NAME:latest
-                echo -e "\x1B[01;32m Publishing $ORGANISATION/$REPO_NAME:latest \x1B[0m"
-                docker push $ORGANISATION/$REPO_NAME:latest
-            fi
-            echo -e "\x1B[01;32m Publishing $BUILD_NAME image \x1B[0m"
-            docker push $BUILD_NAME
+    DOCKER_FILE_PATH="$BASE_PATH/Dockerfile"
+    if [ -f $DOCKER_FILE_PATH ]; then
+        echo -e "\x1B[01;32mFound Docker configuration file at $BASE_PATH \x1B[0m"
+        BASE_PATH=${BASE_PATH#$REPO_BASE_DIRECTORY/}
+        echo $BASE_PATH
+        read ORGANISATION REPO_NAME IMAGE_TAG <<< $(echo $BASE_PATH | awk -F'[/:]' '{print $1,$2,$3}')
+        echo -e "\x1B[01;32mBuilding container from $DOCKER_FILE_PATH file with tag $ORGANISATION/$REPO_NAME:$IMAGE_TAG \x1B[0m"
+        BUILD_NAME="$ORGANISATION/$REPO_NAME:$IMAGE_TAG"
+        echo $BUILD_NAME
+        docker build -t $BUILD_NAME $BUILD_NAME
+        LATEST_IMAGE=$(ls -rd "$REPO_BASE_DIRECTORY/$ORGANISATION/$REPO_NAME"* | head -1)
+        CURRENT_IMAGE="$REPO_BASE_DIRECTORY/$BUILD_NAME"
+        if [ "$LATEST_IMAGE" == "$CURRENT_IMAGE" ]; then
+            echo -e "\x1B[01;32m Updating latest tag for $ORGANISATION/$REPO_NAME \x1B[0m"
+            docker tag $BUILD_NAME $ORGANISATION/$REPO_NAME:latest
         fi
+        echo -e "\x1B[01;32m Publishing $BUILD_NAME image \x1B[0m"
+        docker push $ORGANISATION/$REPO_NAME
     fi
 done
--- python/src/cve_scanner.py
+++ python/src/cve_scanner.py
@@ -3,6 +3,7 @@ try:
   import urllib
   import json
   import sys
+  import pprint
   import os
 except ImportError, err:
   print err
@@ -11,11 +12,10 @@ try:
 except IndexError, ie_err:
   print "Usage: %s <CVE_ID>" %(os.path.basename(__file__))
   sys.exit(1)
-
 cve_url="http://cve.circl.lu/api/cve/%s" %(cve_id)
-
 cve_db=urllib.urlopen(cve_url)
 json_results=json.load(cve_db)
 cve_db.close
-
 print "%s|%s"%(json_results['id'],json_results['summary'])
+for each_capec in json_results['capec']:
+  print "Title of Attack: %s\nSummary: %s\nSolutions:%s"%(each_capec['name'],''.join(each_capec['summary']),''.join(each_capec['solutions']))
