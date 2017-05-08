787563ca9fa1fed984ed4c3bee488dc15c0f4342
Venkat Bumireddy <venkat.bumireddy@phenompeople.com>
2017-05-08 10:18:02 +0530

    adding cms-tenant cookbook

---
+++ cms-tenant-service/metadata.rb
@@ -0,0 +1,7 @@
+name             'cms-tenant-service'
+maintainer       'Phenompeople Pvt Ltd'
+maintainer_email 'admin.squad@phenompeople.com'
+license          'Apache 2.0'
+description      'Installs/Configures cms-tenant-service'
+long_description 'Installs/Configures cms-tenant-service'
+version          '0.1.0'
---
+++ cms-tenant-service/recipes/ecr_undeploy.rb
@@ -0,0 +1,4 @@
+ecr_undeploy "cms_tenant-service undeploy" do
+  vault_name 'services'
+  app_name 'cms_tenant-service'
+end
