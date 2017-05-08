b90829fb1ab6b8a2fcb736d0147111602bc7820e
Venkat Bumireddy <venkat.bumireddy@phenompeople.com>
2017-05-08 10:19:57 +0530

    adding cp-usermanagement cookbook

---
+++ cp-usermanagement/recipes/ecr_deploy.rb
@@ -0,0 +1,4 @@
+ecr_deploy "cp-usermanagement Deploy" do
+  vault_name 'services'
+  app_name 'cp-usermanagement'
+end
---
+++ cp-usermanagement/recipes/ecr_undeploy.rb
@@ -0,0 +1,4 @@
+ecr_undeploy "cp-usermanagement undeploy" do
+  vault_name 'services'
+  app_name 'cp-usermanagement'
+end
