9f4abe58079b4fd2a727ed4aa9ee4a6071f85943
Venkat Bumireddy <venkat.bumireddy@phenompeople.com>
2017-05-08 10:19:01 +0530

    adding cp-facets cookbook

---
+++ cp-facets/recipes/ecr_deploy.rb
@@ -0,0 +1,4 @@
+ecr_deploy "cp-facets Deploy" do
+  vault_name 'services'
+  app_name 'cp-facets'
+end
---
+++ cp-facets/recipes/ecr_undeploy.rb
@@ -0,0 +1,4 @@
+ecr_undeploy "cp-facets undeploy" do
+  vault_name 'services'
+  app_name 'cp-facets'
+end
