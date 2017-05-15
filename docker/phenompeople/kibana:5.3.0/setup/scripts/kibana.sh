93e4c1a7b3cfd17c4c24cbda24a30cabcec35f2c
Rajesh Jonnalagadda <rajesh.jonnalagadda@phenompeople.com>
2017-05-15 16:17:18 +0530

    Updated Kibana container

--- docker/phenompeople/kibana:5.3.0/setup/scripts/kibana.sh
+++ docker/phenompeople/kibana:5.3.0/setup/scripts/kibana.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -o nounset # Treat unset variables as an error
-ELASTICSEARCH=${ELASTICSEARCH:-http://localhost:9200}
+ELASTICSEARCH_URL=${ELASTICSEARCH_URL:-http://localhost:9200}
 KIBANA_INDEX=${KIBANA_INDEX:-.kibana}
 SERVER_HOST=${SERVER_HOST:-localhost}
-sed -ri "s|^(\\#\\s*)?(elasticsearch.url:).*|\\2 '$ELASTICSEARCH'|; \
+sed -ri "s|^(\\#\\s*)?(elasticsearch.url:).*|\\2 '$ELASTICSEARCH_URL'|; \
                 s|^(\\#\\s*)?(server.host:).*|\\2 '$SERVER_HOST'|; \
                 s|^(\\#\\s*)?(kibana.index:).*|\\2 '$KIBANA_INDEX'|; " /opt/kibana/config/kibana.yml
 
