d7b33b2ea6d450cc17942d56a35e9f1bc038e550
Rajesh Jonnalagadda <rajesh.jonnalagadda@phenompeople.com>
2017-05-26 12:30:08 +0530

    Various files pushing to tools

--- bash/src/git_merger.sh
+++ bash/src/git_merger.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 BASE_DIRECTORY=$1
-if [ "$#" -ne 1 ]; then
- echo "Usage: $0 GIT_REPO_DIRECTORY"
+BRANCH_NAME=$2
+if [ "$#" -ne 2 ]; then
+ echo "Usage: $0 GIT_REPO_DIRECTORY BRANCH_NAME"
  exit 1
 fi
 cd $BASE_DIRECTORY
-REMOTE_BRANCHES=$(git branch -r | grep -v master)
-BUILD_FILES="Dockerfile build.sh start-server.sh stop-server.sh"
+REMOTE_BRANCHES=$(git branch -r | grep -v $BRANCH_NAME)
+#BUILD_FILES="Dockerfile build.sh start-server.sh stop-server.sh"
+BUILD_FILES="Dockerfile build.sh setup.sh"
 #echo $REMOTE_BRANCHES
 for EACH_BRANCH in $REMOTE_BRANCHES; do
     OTHER_BRANCH=${EACH_BRANCH#origin/}
-    "Working in $OTHER_BRANCH"
+    echo "Working in $OTHER_BRANCH"
     git checkout $OTHER_BRANCH
-    git checkout master $BUILD_FILES
+    git checkout $BRANCH_NAME $BUILD_FILES
     git add $BUILD_FILES
     git commit -m "Adding Build script files"
     git push origin $OTHER_BRANCH
--- python/src/cve_scanner.py
+++ python/src/cve_scanner.py
@@ -16,6 +16,7 @@ cve_url="http://cve.circl.lu/api/cve/%s" %(cve_id)
 cve_db=urllib.urlopen(cve_url)
 json_results=json.load(cve_db)
 cve_db.close
-print "%s|%s"%(json_results['id'],json_results['summary'])
+#print "%s|%s"%(json_results['id'],json_results['summary'])
 for each_capec in json_results['capec']:
-  print "Title of Attack: %s\nSummary: %s\nSolutions:%s"%(each_capec['name'],''.join(each_capec['summary']),''.join(each_capec['solutions']))
+  # print "Title of Attack: %sSummary: %s\nSolutions:%s"%(each_capec['name'],''.join(each_capec['summary']),''.join(each_capec['solutions']))
+  print "%s|%s|%s|%s|%s"%(json_results['id'],json_results['summary'],each_capec['name'],''.join(each_capec['summary']),''.join(each_capec['solutions']))
