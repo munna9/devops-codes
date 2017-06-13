cc1276a66c6db8da17ec377d4b0dbd9f98aa914a
phenom_akshitha_tekulapalli <akshitha.tekulapalli@phenompeople.com>
2017-06-13 18:24:19 +0530

    resource-server commited

---
+++ resource-server/recipes/ecr_deploy.rb
@@ -0,0 +1,4 @@
+ecr_deploy "resource-server Deploy" do
+  vault_name 'communities'
+  app_name 'login-resource-server'
+end
---
+++ resource-server/recipes/ecr_undeploy.rb
@@ -0,0 +1,4 @@
+ecr_undeploy "resource-server undeploy" do
+  vault_name 'communities'
+  app_name 'login-resource-server'
+end
