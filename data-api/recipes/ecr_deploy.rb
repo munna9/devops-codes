3a7f21f314d50d609852fc0be5ea6b4b3903f1fe
Rajesh Jonnalagadda <rajesh.jonnalagadda@phenompeople.com>
2017-07-06 16:08:31 +0530

    data-api cookbook initial import

---
+++ data-api/recipes/ecr_deploy.rb
@@ -0,0 +1,4 @@
+ecr_deploy "data-api Deploy" do
+  vault_name 'services'
+  app_name 'data-api'
+end
---
+++ data-api/recipes/ecr_undeploy.rb
@@ -0,0 +1,4 @@
+ecr_undeploy "data-api undeploy" do
+  vault_name 'services'
+  app_name 'data-api'
+end
