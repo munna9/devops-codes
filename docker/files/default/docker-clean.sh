b5d5bdfe7fc5709bf7ccab21761ef5d1d74f2e71
Rajesh Jonnalagadda <rajesh.jonnalagadda@phenompeople.com>
2017-08-15 05:47:13 +0530

    Fixing docker-clean

--- docker/files/default/docker-clean.sh
+++ docker/files/default/docker-clean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 IMAGES_TO_REMOVE=$(docker images -f "dangling=true" -q)
-if [ -z "IMAGES_TO_REMOVE"]; then
+if [ -z "IMAGES_TO_REMOVE" ]; then
   docker rmi $IMAGES_TO_REMOVE
 fi
